3fa4ef5ed0ece02f818e1cc0335704c6a2b58ca4
Rune Sagevik <rune.sagevik@norsvin.no>
2023-08-02 15:10:00 +0200

    0.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="testpackage",  # should match the package folder
     packages=["testpackage"],  # should match the package folder
-    version="0.0.1",  # important for updates
+    version="0.0.3",  # important for updates
     license="MIT",  # should match your chosen license
     description="Testing installation of Package",
     long_description=long_description,  # loads your README.md
